c359588f7c50aa34fd81487c70688e97705d709b
z123fg <zyz1029@gmail.com>
2023-03-08 09:37:36 -0600

    allocate id

--- src/Tree.ts
+++ src/Tree.ts
@@ -42,7 +42,27 @@
     [1,2][3,5]
     [2,3]
     [2,3][1,2]
+
+    [1,1]
+    [1,2]
+    [1,2][3,5]
+    [1,3]
+    [1,3][1,2]
 */
+class Node {
+    children: any;
+    data: any;
+    constructor(data: any) {
+        this.children = null;
+        this.data = data;
+    }
+}
+class Tree {
+    root: Node;
+    constructor() {
+        this.root = new Node("root");
+    }
+}
 
 export type Comparator = 1 | -1 | 0;
 
@@ -55,42 +75,112 @@ export interface Atom {
 }
 
 const allocateId = (id1: number[], id2: number[]): number[] => {
-    return []
-}
+    return [];
+};
 
-const generateId = (id1: positionDigit[], id2: positionDigit[], curIndex=0,siteId:number) => {
+/* const generateId = (id1: positionDigit[], id2: positionDigit[], curIndex = 0, siteId: number) => {
+    if(curIndex)
     const head1 = id1[curIndex];
     const head2 = id2[curIndex];
     const siteId1 = head1.siteId;
     const siteId2 = head2.siteId;
     const index1 = head1.index;
     const index2 = head2.index;
-    if(index1 < index2){
-        if(siteId > id1[curIndex].siteId){
-
-        }else{
-
+    if (index2 - index1 > 1) {
+        if (siteId > siteId1) {
+            if (siteId < siteId2) {
+                //allocateId() from [id1 id2]
+            } else {
+                //allocateId() from [id1 id2)
+            }
+        } else {
+            if (siteId < siteId2) {
+                //allocateId() from (id1 id2]
+            } else {
+                //allocateId() from (id1 id2)
+            }
         }
-    }else if(index1 === index2){
-        if(siteId1 === siteId1){
-            generateId(id1, id2, ++curIndex, siteId)
-        }else if(siteId1 < siteId2){
-            //allocateId() from id1 to infinity
-        }else{
-            throw Error("invalid id order!")
+    } else if (index1 === index2) {
+        if (siteId1 === siteId1) {
+            generateId(id1, id2, ++curIndex, siteId);
+        } else if (siteId1 < siteId2) {
+            //allocateId() from (id1 infinity)
+        } else {
+            throw Error("invalid id order!(1)");
         }
-    }else{
-
+    } else {
+        throw Error("invalid id order!(2)");
     }
-}
+}; */
+
+const generateId = (id1: positionDigit[], id2: positionDigit[], siteId: number) => {
+    let head1;
+    let head2;
+    let siteId1;
+    let siteId2;
+    let index1;
+    let index2;
+    for (let i = 0; i < Math.min(id1.length, id2.length); i++) {
+        head1 = id1[i];
+        head2 = id2[i];
+        siteId1 = head1.siteId;
+        siteId2 = head2.siteId;
+        index1 = head1.index;
+        index2 = head2.index;
+        if (index2 - index1 > 1) {
+            if (siteId > siteId1) {
+                if (siteId < siteId2) {
+                    //return allocateId() from [id1 id2]
+                } else {
+                    //return allocateId() from [id1 id2)
+                }
+            } else {
+                if (siteId < siteId2) {
+                    //return allocateId() from (id1 id2]
+                } else {
+                    //return allocateId() from (id1 id2)
+                }
+            }
+        } else if (index2 - index1 === 1) {
+            if (siteId > siteId1) {
+                if (siteId < siteId2) {
+                    //return allocateId() from [id1 id2]
+                } else {
+                    //return allocateId() from [id1 id2)
+                }
+            } else {
+                if (siteId < siteId2) {
+                    //return allocateId() from (id1 id2]
+                } else {
+                    continue;
+                }
+            }
+        } else if (index1 === index2) {
+            /* if (siteId1 === siteId1) {
+                continue;
+            } else if (siteId1 < siteId2) {
+                //return allocateId() from (id1 infinity)
+            } else {
+                throw Error("invalid id order!(1)");
+            } */
+            if (siteId1 > siteId2) {
+                throw Error("invalid id order!(2)");
+            }
+            if (siteId < siteId2 && siteId > siteId1) {
+                //return allocateId() from [id1, id2];
+            } else {
+                continue;
+            }
+        } else {
+            throw Error("invalid id order!(3)");
+        }
+    }
+    //allocateId(0)
+};
 
 export const init = 0;
 
-const compareIds = (
-    id1: positionDigit[],
-    id2: positionDigit[],
-    curIndex = 0
-): Comparator => {
+const compareIds = (id1: positionDigit[], id2: positionDigit[], curIndex = 0): Comparator => {
     if (curIndex >= id1.length && curIndex < id2.length) {
         return -1;
     } else if (curIndex < id1.length && curIndex >= id2.length) {
@@ -104,17 +194,17 @@ const compareIds = (
     const siteId2 = head2.siteId;
     const index1 = head1.index;
     const index2 = head2.index;
-    if(index1 > index2){
+    if (index1 > index2) {
         return 1;
-    }else if(index1 < index2){
+    } else if (index1 < index2) {
         return -1;
-    }else{
-        if(siteId1 > siteId2){
+    } else {
+        if (siteId1 > siteId2) {
             return 1;
-        }else if(siteId1 < siteId2){
+        } else if (siteId1 < siteId2) {
             return -1;
-        }else{
-            return compareIds(id1, id2, ++curIndex)
+        } else {
+            return compareIds(id1, id2, ++curIndex);
         }
     }
 };
